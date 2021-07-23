08dcb6a6e1cc11fab99234eb7f897e96ca21083d
ryan <bookwyrm31415@gmail.com>
2021-07-24 11:48:12 +1200

    Everything basic working

--- public/app.js
+++ public/app.js
@@ -5,16 +5,20 @@ const db = firebase.firestore();
 let collectionRef;
 let unsubscribe;
 
-collectionRef = db.collection('articles')
+collectionRef = db.collection('articles').orderBy('timestamp','desc')
 
 
-unsubscribe = collectionRef
+function search(ele) {
+    if(event.key === 'Enter') {
+        searchTerm.innerHTML = `<p> ${ele.value}</p>`;        
 
-        .where("wordList", "array-contains", "Covid")
+        unsubscribe = collectionRef
+
+        .where("wordList", "array-contains", `${ele.value.toLowerCase()}`)
         .onSnapshot( querySnapshot => {
             const items = querySnapshot.docs.map(doc =>{
 
-                return `<li> ${doc.data().Headline}</li>`
+                return `<li><a href = ${doc.data().Link}> ${doc.data().Headline}</a>:<br>  ${doc.data().Intro} <br> ${doc.data().timestamp.toDate()}</li>`
             });
 
             articleList.innerHTML = items.join("\n")
@@ -24,3 +28,9 @@ unsubscribe = collectionRef
 
 
         )
+    }
+}
+
+
+
+        
