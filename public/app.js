092ea3a95a16a5f448f143c5c081f9f63b903fc6
ryan <bookwyrm31415@gmail.com>
2021-07-24 12:10:14 +1200

    fixed indentation

--- public/app.js
+++ public/app.js
@@ -5,32 +5,31 @@ const db = firebase.firestore();
 let collectionRef;
 let unsubscribe;
 
-collectionRef = db.collection('articles').orderBy('timestamp','desc')
+collectionRef = db.collection('articles').orderBy('timestamp', 'desc')
 
 
 function search(ele) {
-    if(event.key === 'Enter') {
-        searchTerm.innerHTML = `<p> ${ele.value}</p>`;        
+    if (event.key === 'Enter') {
+        searchTerm.innerHTML = `<p> ${ele.value}</p>`;
 
         unsubscribe = collectionRef
 
-        .where("wordList", "array-contains", `${ele.value.toLowerCase()}`)
-        .onSnapshot( querySnapshot => {
-            const items = querySnapshot.docs.map(doc =>{
+            .where("wordList", "array-contains", `${ele.value.toLowerCase()}`)
+            .onSnapshot(querySnapshot => {
+                const items = querySnapshot.docs.map(doc => {
 
-                return `<li><a href = ${doc.data().Link}> ${doc.data().Headline}</a>:<br>  ${doc.data().Intro} <br> ${doc.data().timestamp.toDate()}</li>`
-            });
+                    return `<li><a href = ${doc.data().Link}> ${doc.data().Headline}</a>:<br>  ${doc.data().Intro} <br> ${doc.data().timestamp.toDate()}</li>`
+                });
 
-            articleList.innerHTML = items.join("\n")
-
-
-        }
+                articleList.innerHTML = (items.length > 0) ? items.join("\n") : "Looks like nothing is there sorry"
 
 
-        )
+            }
+
+
+            )
     }
 }
 
 
 
-        
